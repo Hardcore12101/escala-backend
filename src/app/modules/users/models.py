68e1f73b7e0829c3a876767c5ca55bd9a3d1e3c8
unknown <lucasmity9@gmail.com>
2026-01-31 09:04:59 -0300

    fix aaaa

--- src/app/modules/users/models.py
+++ src/app/modules/users/models.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import UUID
 from uuid import uuid4
+from src.app.models.company import Company
 
 class User(Base):
     __tablename__ = "users"
@@ -17,3 +18,9 @@ class User(Base):
     is_active = Column(Boolean, default=True)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    companies = relationship(
+        "Company",
+        secondary=user_company_role,
+        back_populates="users",
+    )
