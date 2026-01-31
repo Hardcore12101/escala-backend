5cb794a8f6ad27f168044ed790cee1e16401bc9e
unknown <lucasmity9@gmail.com>
2026-01-31 09:28:40 -0300

    fix aaaa

---
+++ alembic/versions/d36c21687e3c_align_users_table_role_is_active.py
@@ -0,0 +1,34 @@
+"""align users table (role, is_active)
+
+Revision ID: d36c21687e3c
+Revises: ace67cbcc6db
+Create Date: 2026-01-31 09:20:29.299153
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'd36c21687e3c'
+down_revision: Union[str, Sequence[str], None] = 'ace67cbcc6db'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "users",
+        sa.Column("role", sa.String(), nullable=False, server_default="user"),
+    )
+    op.add_column(
+        "users",
+        sa.Column("is_active", sa.Boolean(), nullable=False, server_default=sa.true()),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("users", "is_active")
+    op.drop_column("users", "role")
--- src/app/database/association_roles.py
+++ src/app/database/association_roles.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Table, Column, Integer, ForeignKey, String
 from src.app.database.base import Base
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
 
 user_company_role = Table(
     "user_company_role",
     Base.metadata,
-    Column("user_id", ForeignKey("users.id"), primary_key=True),
-    Column("company_id", ForeignKey("companies.id"), primary_key=True),
+    Column("user_id", UUID(as_uuid=True), ForeignKey("users.id"), primary_key=True),
+    Column("company_id", Integer, ForeignKey("companies.id"), primary_key=True),
     Column("role", String(50), nullable=False),
 )
