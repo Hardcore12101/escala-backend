bdce86e3181a1e788b136cb4c34b23f3a8e906ff
unknown <lucasmity9@gmail.com>
2026-02-01 12:54:33 -0300

    fix: aaaaa

--- src/app/modules/companies/router.py
+++ src/app/modules/companies/router.py
@@ -42,7 +42,7 @@ def list_companies(
     db: Session = Depends(get_db),
     user: User = Depends(get_current_user),
 ):
-    return service.list_companies(
+    return list_companies(
         db=db,
         user=user,
         page=page,
