8bef4e95af4b48543f66740ef12f0a5e387c5c7e
unknown <lucasmity9@gmail.com>
2026-02-01 12:19:40 -0300

    fix: aaaaa

---
+++ alembic/versions/cd93d4f3bfd8_audit_logs_entity_id_uuid_safe_migration.py
@@ -0,0 +1,40 @@
+"""audit_logs entity_id uuid safe migration
+
+Revision ID: cd93d4f3bfd8
+Revises: 3d2aaa5c139a
+Create Date: 2026-02-01 12:18:55.718912
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = "cd93d4f3bfd8"
+down_revision = "4490072962f8"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # 1. cria nova coluna
+    op.add_column(
+        "audit_logs",
+        sa.Column("entity_uuid", postgresql.UUID(as_uuid=True), nullable=True),
+    )
+
+    # 2. (opcional) se quiser, pode copiar apenas onde fizer sentido
+    # NÃO tente converter int -> uuid
+
+    # 3. remove coluna antiga
+    op.drop_column("audit_logs", "entity_id")
+
+    # 4. renomeia para manter contrato
+    op.alter_column("audit_logs", "entity_uuid", new_column_name="entity_id")
+
+
+def downgrade():
+    op.add_column(
+        "audit_logs",
+        sa.Column("entity_id", sa.Integer(), nullable=True),
+    )
+    op.drop_column("audit_logs", "entity_uuid")
