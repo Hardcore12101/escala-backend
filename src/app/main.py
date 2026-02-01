a8528061f9f407a6794082cba9c8ae9543e6f87e
unknown <lucasmity9@gmail.com>
2026-02-01 08:55:36 -0300

    fix aaaa

--- src/app/main.py
+++ src/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
        "http://localhost:5173",
        "https://escala-backend-production.up.railway.app",
-    ]
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
