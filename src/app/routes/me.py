e987e4ce83499e78f14858eab2606b61d64f9f79
unknown <lucasmity9@gmail.com>
2026-02-07 18:51:06 -0300

    aaaa

--- src/app/routes/me.py
+++ src/app/routes/me.py
@@ -1,14 +1,12 @@
 from fastapi import APIRouter, Depends
 from src.app.modules.auth.dependencies import get_current_user
 from src.app.modules.users.models import User
-from src.app.core.deps import get_current_context
 
 router = APIRouter(prefix="/me", tags=["Me"])
 
+
 @router.get("")
-def me(ctx = Depends(get_current_context)):
-    return {
-        "user": ctx.user.email,
-        "company": ctx.company.name,
-        "role": ctx.role,
-    }
+def read_me(
+    current_user: User = Depends(get_current_user),
+):
+    return current_user
